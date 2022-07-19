aa9daccaa13d365159418f7752562ca52253b09b
View <1140182402@qq.com>
2022-07-19 14:20:55 +0800

    更新我的页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -3,39 +3,12 @@ Component({
     addGlobalClass: true,
   },
   data: {
-    starCount: 0,
-    forksCount: 0,
-    visitTotal: 0,
+    starCount: 1,
+    forksCount: 24,
+    visitTotal: 1,
   },
   attached() {
-    console.log("success")
-    let that = this;
-    wx.showLoading({
-      title: '数据加载中',
-      mask: true,
-    })
-    let i = 0;
-    numDH();
-    function numDH() {
-      if (i < 20) {
-        setTimeout(function () {
-          that.setData({
-            starCount: i,
-            forksCount: i,
-            visitTotal: i
-          })
-          i++
-          numDH();
-        }, 20)
-      } else {
-        that.setData({
-          starCount: that.coutNum(3000),
-          forksCount: that.coutNum(484),
-          visitTotal: that.coutNum(24000)
-        })
-      }
-    }
-    wx.hideLoading()
+ 
   },
   methods: {
     coutNum(e) {
