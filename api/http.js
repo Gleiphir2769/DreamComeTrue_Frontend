80a0dcca86741dd2f7222d9d3cf892ec0eeec6ac
View <1140182402@qq.com>
2022-07-23 23:02:07 +0800

    update http

--- api/http.js
+++ api/http.js
@@ -1,8 +1,24 @@
-function get(url) {
+const headers={}
+const getToken=()=>{
+  let token=wx.getStorageSync('token')
+  if(token)
+    token=`Bearer ${token}`
+  return token
+}
+const getHeader=(extraHeaders)=>{
+    let h={"Token":getToken()}
+    for(let item in extraHeaders){
+      h[item]=headers[item]
+    }
+    return h
+}
+
+function get(url, headers={}) {
     return new Promise((resolve, reject) => {
         wx.request({
             url,
             method:'get',
+            headers:getHeader(headers),
             success: (res) => {
                 resolve(res)
             },
@@ -13,11 +29,12 @@ function get(url) {
     })
 }
 
-function post(url,data) {
+function post(url,data,headers={}) {
   return new Promise((resolve, reject) => {
       wx.request({
           url,
           method:'post',
+          headers:getHeader(headers),
           success: (res) => {
               resolve(res)
           },
--- pages/login/login.js
+++ pages/login/login.js
@@ -9,7 +9,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    api.login()
+    api.login({username:'',password:''}).then(data=>{
+     if(data.code===50004){
+       alert(data.message)
+     }
+    })
   },
 
   /**
