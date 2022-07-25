e3820595cbf4b43d2969b8ab5eb03689a98ad8e9
View <1140182402@qq.com>
2022-07-25 11:04:29 +0800

    优化my页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -7,61 +7,69 @@ Page({
     projectCount: 1,
     teamCount: 24,
     timeCount: 0,
-    currentRole: 0,
-    roles: ['志愿者', '志愿队伍', '志愿中心'],
-    colors:['orange','olive','red'],
+    roles: ['user', 'master', 'admin'],
+    rolesDict:{
+      "user":"志愿者",
+      "master":"志愿队伍",
+      "admin":"支援中心"
+    },
+    colors:{
+      "user":"orange",
+      "master":"olive",
+      "admin":"red"
+    },
     menus: [],
     menuData: [{
       title: '个人资料',
-      role: '志愿者'
+      role: 'user'
     },{
         title: '我的队伍',
-        role: '志愿者',
+        role: 'user',
         url:'/pages/my/project/project?p=1&title=我的队伍'
       },
       {
         title: '我的项目',
-        role: '志愿者',
+        role: 'user',
         url:'/pages/my/project/project?p=2&title=我的项目'
       },
       // {
       //   title: '排行榜',
-      //   role: '志愿者',
+      //   role: 'user',
       //   url:'/pages/my/time/time'
       // },
       {
         title: '我的成员',
-        role: '志愿队伍',
+        role: 'master',
         url:'/pages/my/team/member/member'
       },
       {
         title: '加入队伍审核',
-        role: '志愿队伍',
+        role: 'master',
         url:'/pages/my/project/project?p=3&title=成员审核'
       },
       {
         title: '加入项目审核',
-        role: '志愿队伍',
+        role: 'master',
         url:'/pages/my/project/project?p=4&title=成员审核'
       },
       {
         title: '项目申请',
-        role: '志愿队伍',
+        role: 'master',
         url:'/pages/my/team/projectApplication/projectApplication'
       },
       {
         title: '我的项目',
-        role: '志愿队伍',
+        role: 'master',
         url:'/pages/my/team/myProject/myProject'
       },
       {
         title: '队伍审核',
-        role: '志愿中心',
+        role: 'admin',
         url:'/pages/my/project/project?p=6&title=队伍审核'
       },
       {
         title: '项目审核',
-        role: '志愿中心',
+        role: 'admin',
         url:'/pages/my/project/project?p=7&title=项目审核'
       }
     ],
@@ -122,6 +130,7 @@ Page({
     let self=this;
     if (uid) {
       let role=wx.getStorageSync('role')
+      console.log(role)
       api.getUserInfo(uid).then(data => {
         data=data.data.data
         self.setData({
